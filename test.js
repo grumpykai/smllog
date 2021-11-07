e8954b5c06c64feca11605791453ec3595687daf
Kai Merklein <kai.merklein@web.de>
2021-11-07 17:41:58 +0100

    promise

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var SerialPort = require("serialport");
 
-SerialPort.list(function (err, ports) {
+SerialPort.list().then((ports) => {
   ports.forEach(function (port) {
     console.log(port.comName, port.pnpId, port.manufacturer); // or console.log(port)
   });
