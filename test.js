31ba03c484d0c26f141d144dc5f2d17f9a5a0e30
Kai Merklein <kai.merklein@web.de>
2021-11-07 22:01:22 +0100

    read-1

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
 var SerialPort = require("serialport");
 const Delimiter = require("@serialport/parser-delimiter");
 
+const registerDelimiters = {
+  highTariff: Uint8Array.from([
+    0x77, 0x07, 0x01, 0x00, 0x01, 0x08, 0x01, 0xff, 0x01, 0x01, 0x62, 0x1e,
+    0x52, 0xff, 0x59,
+  ]),
+  lowTariff: Uint8Array.from([
+    0x77, 0x07, 0x01, 0x00, 0x01, 0x08, 0x02, 0xff, 0x01, 0x01, 0x62, 0x1e,
+    0x52, 0xff, 0x59,
+  ]),
+  feedIn: Uint8Array.from([
+    0x77, 0x07, 0x01, 0x00, 0x02, 0x08, 0x01, 0xff, 0x01, 0x01, 0x62, 0x1e,
+    0x52, 0xff, 0x59,
+  ]),
+};
+
 SerialPort.list().then((ports) => {
   ports.forEach(function (port) {
     console.log(port.comName, port.pnpId, port.manufacturer); // or console.log(port)
@@ -31,39 +46,14 @@ const parser = port.pipe(
   new Delimiter({ delimiter: [0x1b, 0x1b, 0x1b, 0x1b] })
 );
 parser.on("data", (buf) => {
-  //console.log(data)
-  //   const values = Buffer.from(buf).values();
-  let index = buf.indexOf(
-    Uint8Array.from([
-      0x77, 0x07, 0x01, 0x00, 0x01, 0x08, 0x01, 0xff, 0x01, 0x01, 0x62, 0x1e,
-      0x52, 0xff, 0x59,
-    ])
-  );
-  let reading = 0;
-  if (index >= 0) {
-    for (let i = 0; i < 8; i++) {
-      reading = reading * 256 + buf[index + 15 + i];
-    }
-    console.log("HT at " + index + " Reading: " + reading);
+  let registerHigh = readMeter(buf, registerDelimiters.highTariff);
+  if (registerHigh >= 0) {
+    let registerLow = readMeter(buf, registerDelimiters.lowTariff);
+    let registerFeedIn = readMeter(buf, registerDelimiters.feedIn);
+    console.log(
+      `HT: ${registerHigh}, NT: ${registerLow}, Feed: ${registerFeedIn}`
+    );
   }
-  //   console.log(
-  //     "NT at " +
-  //       buf.indexOf(
-  //         Uint8Array.from([
-  //           0x77, 0x07, 0x01, 0x00, 0x01, 0x08, 0x02, 0xff, 0x01, 0x01, 0x62,
-  //           0x1e, 0x52, 0xff, 0x59,
-  //         ])
-  //       )
-  //   );
-  //   console.log(
-  //     "EN at " +
-  //       buf.indexOf(
-  //         Uint8Array.from([
-  //           0x77, 0x07, 0x01, 0x00, 0x02, 0x08, 0x01, 0xff, 0x01, 0x01, 0x62,
-  //           0x1e, 0x52, 0xff, 0x59,
-  //         ])
-  //       )
-  //   );
 });
 
 // int delHT[] = {
@@ -72,3 +62,15 @@ parser.on("data", (buf) => {
 //     0x77, 0x07, 0x01, 0x00, 0x01, 0x08, 0x02, 0xFF, 0x01, 0x01, 0x62, 0x1E, 0x52, 0xFF, 0x59 };
 //   int delEN[] = {
 //     0x77, 0x07, 0x01, 0x00, 0x02, 0x08, 0x01, 0xFF, 0x01, 0x01, 0x62, 0x1E, 0x52, 0xFF, 0x59 };
+
+function readMeter(buf, delimiter) {
+  let index = buf.indexOf(delimiter);
+  let reading = -1;
+  if (index >= 0) {
+    for (let i = 0; i < 8; i++) {
+      reading = reading * 256 + buf[index + 15 + i];
+    }
+    console.log("HT at " + index + " Reading: " + reading);
+  }
+  return reading;
+}
