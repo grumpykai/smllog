5cbbc709d5cfb681e394ac946ffe0cf169916cef
Kai Merklein <kai.merklein@web.de>
2021-11-07 17:37:59 +0100

    list

--- test.js
+++ test.js
@@ -1,16 +1,7 @@
 var SerialPort = require("serialport");
-var port = new SerialPort("/dev/ttyUSB0");
 
-port.on("open", function () {
-  port.write("main screen turn on", function (err) {
-    if (err) {
-      return console.log("Error on write: ", err.message);
-    }
-    console.log("message written");
+SerialPort.list(function (err, ports) {
+  ports.forEach(function (port) {
+    console.log(port.comName, port.pnpId, port.manufacturer); // or console.log(port)
   });
-});
-
-// open errors will be emitted as an error event
-port.on("error", function (err) {
-  console.log("Error: ", err.message);
 });
