7577d8b20ee21b442b2723357d7483e2f215022d
Kai Merklein <kai.merklein@web.de>
2021-11-07 17:43:46 +0100

    serial-1

--- test.js
+++ test.js
@@ -5,3 +5,28 @@ SerialPort.list().then((ports) => {
     console.log(port.comName, port.pnpId, port.manufacturer); // or console.log(port)
   });
 });
+
+const port = new SerialPort("/dev/ttyUSB0", { autoOpen: false });
+
+port.open(function (err) {
+  if (err) {
+    return console.log("Error opening port: ", err.message);
+  }
+
+  // Because there's no callback to write, write errors will be emitted on the port:
+  port.write("main screen turn on");
+});
+
+// The open event is always emitted
+port.on("open", function () {
+  // open logic
+});
+
+port.on("readable", function () {
+  console.log("Data:", port.read());
+});
+
+// Switches the port into "flowing mode"
+port.on("data", function (data) {
+  console.log("Data:", data);
+});
