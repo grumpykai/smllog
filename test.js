6cd2d2dcc2db4319eb1686f342d3e23a63d3815f
Kai Merklein <kai.merklein@web.de>
2021-11-09 09:11:55 +0100

    logging reworked

--- test.js
+++ test.js
@@ -156,7 +156,7 @@ function readMeter(buf, delimiter, byteCount = 8) {
 
 function countRegistersForUpload(deviceParamConfig) {
   let count = 0;
-  for (const device in deviceParamConfig) {
+  for (const device of deviceParamConfig) {
     for (const register of device.registers) {
       if (register.urlParam) count++;
     }
